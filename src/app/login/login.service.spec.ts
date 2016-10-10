93391034c99bb505afa9d5976dcf3e80c56e50f5
Tiago Resende <tiagor87@gmail.com>
2016-10-10 20:20:36 -0300

    Descomenta.

--- src/app/login/login.service.spec.ts
+++ src/app/login/login.service.spec.ts
@@ -1,15 +1,15 @@
-/*import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+//import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { LoginService } from './login.service';
 
 describe('LoginService', () => {
-  let fixture: ComponentFixture<LoginService>;
+  //let fixture: ComponentFixture<LoginService>;
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [LoginService]
-    });
-    fixture = TestBed.createComponent(LoginService);
+    //TestBed.configureTestingModule({
+    //  providers: [LoginService]
+    //});
+    //fixture = TestBed.createComponent(LoginService);
   })
   it('deve ser possível entrar pelo email e senha', done => {
     done();
   })
-})*/
+})
